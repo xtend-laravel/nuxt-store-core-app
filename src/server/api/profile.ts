91955d355baed5f2b7b373da967888d3df0c021d
Adam Lee <adam@codelabx.ltd>
2023-02-10 22:44:29 +0000

    Implemented new api nitro composable

--- src/server/api/profile.ts
+++ src/server/api/profile.ts
@@ -1,12 +1,9 @@
-import { H3Event, getCookie } from 'h3'
-
-const baseUrl = process.env.NUXT_APP_HUB_BASE_URL
+import { H3Event } from 'h3'
+import useNitroApi from '~/composables/useNitroApi'
 export default defineEventHandler(async (event: H3Event) => {
-  return await $fetch(`${baseUrl}/api/restify/profile`, {
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${getCookie(event, 'token') || ''}`,
-    },
+  return await useNitroApi({
+    event,
+    endpoint: '/api/restify/profile',
+    requiresAuth: true,
   })
 })
