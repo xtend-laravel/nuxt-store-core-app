767be550a66db9d2526d8d8ff0d7c0dcdd2c1745
Adam Lee <adam@codelabx.ltd>
2023-03-01 02:56:16 +0000

    clean up formattedPrice

--- src/composables/useFormattedPrice.ts
+++ src/composables/useFormattedPrice.ts
@@ -1,35 +1,34 @@
-import { ComputedRef, Ref, computed, isRef, ref } from "vue";
+import { ComputedRef, Ref, computed, isRef } from 'vue'
 
 interface FormattedPrice {
   formatPrice: (
     price: Ref<number> | number,
     discountPercentage?: number,
     priceDivisor?: number
-  ) => ComputedRef<string>;
+  ) => ComputedRef<string>
 }
 
 export default function useFormattedPrice(
-  currencyCode: string
+  currencyCode: string,
 ): FormattedPrice {
   const formatPrice = (
     price: Ref<number> | number,
-    discountPercentage: number = 0,
-    priceDivisor: number = 1
+    discountPercentage = 0,
+    priceDivisor = 1,
   ): ComputedRef<string> => {
-    let priceValue = isRef(price) ? price.value : price;
+    let priceValue = isRef(price) ? price.value : price
     if (discountPercentage) {
-      priceValue = priceValue - priceValue * (discountPercentage / 100);
+      priceValue = priceValue - priceValue * (discountPercentage / 100)
     }
-    const formattedPrice = computed(() =>
-      new Intl.NumberFormat("en-US", {
-        style: "currency",
+    return computed(() =>
+      new Intl.NumberFormat('en-US', {
+        style: 'currency',
         currency: currencyCode,
-      }).format(priceValue / priceDivisor)
-    );
-    return formattedPrice;
-  };
+      }).format(priceValue / priceDivisor),
+    )
+  }
 
   return {
     formatPrice,
-  };
+  }
 }
