0810cc9d3a2bf8e864f95951ca5e585b45159070
Adam Lee <adam@codelabx.ltd>
2023-04-06 02:37:39 +0100

    remove vite ignore to debug

--- src/composables/useComponent.ts
+++ src/composables/useComponent.ts
@@ -1,17 +1,15 @@
-import { Component, defineAsyncComponent } from 'vue'
+import type { Component } from 'vue'
+import { defineAsyncComponent } from 'vue'
 
 type ComponentLoader = () => Promise<{ default: Component }>
 
 export default function useComponent(componentPath: string, rootComponentDir = '/_nuxt/components/'): ComponentLoader {
-  const fallback = defineAsyncComponent(
-    () => import(/* @vite-ignore */ `../components/${componentPath}.vue`),
-  ) as ComponentLoader
+  const fallback = defineAsyncComponent(() => import(`../components/${componentPath}.vue`)) as ComponentLoader
   return defineAsyncComponent({
     loader: async () => {
       try {
-        return await import(/* @vite-ignore */ `${rootComponentDir}${componentPath}.vue`)
-      }
-      catch (error) {
+        return await import(`${rootComponentDir}${componentPath}.vue`)
+      } catch (error) {
         return fallback
       }
     },
