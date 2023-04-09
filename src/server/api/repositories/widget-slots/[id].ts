2e2a6eb991e7eca50bfff89f9bfafb07ebf44f02
Adam Lee <adam@codelabx.ltd>
2023-04-09 17:33:07 +0100

    Rename widget slot route to id

--- src/server/api/repositories/widget-slots/[id].ts
+++ src/server/api/repositories/widget-slots/[id].ts
@@ -1,10 +1,11 @@
-import { H3Event } from 'h3'
+import type { H3Event } from 'h3'
 import useNitroApi from '../../../../composables/useNitroApi'
 
 export default defineEventHandler(async (event: H3Event) => {
   const params: any = event.context.params
+  console.log('params', params)
   return await useNitroApi({
     event,
-    endpoint: '/api/restify/widget-slots/1?include=widgets',
+    endpoint: `/api/restify/widget-slots/${params.id}?include=widgets`,
   })
 })
