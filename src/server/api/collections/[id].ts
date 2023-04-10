4395db4cb648600de52975f8453f7e9faf210a5c
Adam Lee <adam@codelabx.ltd>
2023-04-10 13:48:47 +0100

    support for nitro query params

--- src/server/api/collections/[id].ts
+++ src/server/api/collections/[id].ts
@@ -3,8 +3,10 @@ import useNitroApi from '../../../composables/useNitroApi'
 
 export default defineEventHandler(async (event: H3Event) => {
   const params: any = event.context.params
+  let query: any = getQuery(event)
+  query = new URLSearchParams(query).toString()
   return await useNitroApi({
     event,
-    endpoint: `/api/restify/collections/${params.slug}`,
+    endpoint: `/api/restify/collections/${params.id}?${query}`,
   })
 })
