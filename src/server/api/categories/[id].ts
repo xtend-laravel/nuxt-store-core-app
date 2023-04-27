42871f28be6a44ea90a2183a0f2ef238133b2fe9
bangnokia <bangnokia@gmail.com>
2023-04-28 05:54:30 +0700

    add query string

--- src/server/api/categories/[id].ts
+++ src/server/api/categories/[id].ts
@@ -3,8 +3,10 @@ import useNitroApi from '../../../composables/useNitroApi'
 
 export default defineEventHandler(async (event: H3Event) => {
   const params: any = event.context.params
+  const query = getQuery(event)
   return await useNitroApi({
     event,
     endpoint: `/api/restify/categories/${params.id}`,
+    query
   })
 })
