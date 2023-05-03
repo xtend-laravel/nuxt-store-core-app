2e4de0450abc6f75a07e21f28caf52ad1bed851a
Adam Lee <adam@codelabx.ltd>
2023-05-03 17:46:36 +0100

    new products + filters api

---
+++ src/server/api/new-products.ts
@@ -0,0 +1,12 @@
+import type { H3Event } from 'h3'
+import useNitroApi from '../../composables/useNitroApi'
+
+export default defineEventHandler(async (event: H3Event) => {
+  const query = getQuery(event)
+  return await useNitroApi({
+    event,
+    action: 'GET',
+    endpoint: `/api/restify/new-products`,
+    query,
+  })
+})
