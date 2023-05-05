7386c4e73f0c2a2242ab1d388aa064dcd094c8b4
Adam Lee <adam@codelabx.ltd>
2023-05-05 16:00:59 +0100

    Qty in cart under price

--- src/components/core/store/cart/items/LineItem.vue
+++ src/components/core/store/cart/items/LineItem.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { Popconfirm } from 'ant-design-vue'
-import Multilingual from '~/components/core/store/Multilingual.vue'
 import IconMinus from '~icons/carbon/subtract'
 import IconAdd from '~icons/carbon/add'
+import Multilingual from '~/components/core/store/Multilingual.vue'
 import { useCartStore } from '~/store/cart'
 
 const props = defineProps<{
@@ -34,10 +34,6 @@ async function increaseQuantity(item: any) {
 async function decreaseQuantity(item: any) {
   if (item.quantity > 1) {
     item.quantity--
-    if (item.quantity === 0) {
-      await removeLine(item)
-      return
-    }
     await cartStore.updateQuantity(item.id, item.quantity)
   }
 }
@@ -46,7 +42,7 @@ async function decreaseQuantity(item: any) {
 <template>
   <div class="relative shrink-0">
     <span
-      class="text-xxs absolute left-1 top-1 flex h-5 w-5 items-center justify-center rounded-full border bg-white font-semibold text-gray-500 shadow sm:-right-2 sm:-top-2"
+      class="text-xxs absolute left-1 top-1 flex h-5 w-5 items-center justify-center rounded-full border bg-black/50 font-semibold text-white shadow sm:-right-2 sm:-top-2"
       v-text="item.quantity"
     />
     <img
@@ -55,35 +51,11 @@ async function decreaseQuantity(item: any) {
       :src="item.product.images.thumbnail"
       :alt="item.product.name"
     />
-    <div class="mt-2 flex items-center">
-      <button
-        type="button"
-        class="z-20 flex h-6 w-6 items-center justify-center border border-r-0 border-neutral-200 hover:bg-gray-100"
-        @click="decreaseQuantity(item)"
-      >
-        <IconMinus class="h-2 w-2" />
-      </button>
-      <input
-        v-model="item.quantity"
-        type="number"
-        name="quantity"
-        class="focus:border-brand-600 focus:ring-brand-600 z-10 inline-flex h-6 w-10 border border-neutral-200 px-2 py-2 text-center text-xs [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
-        inputmode="numeric"
-        disabled
-      />
-      <button
-        type="button"
-        class="z-20 flex h-6 w-6 items-center justify-center border border-l-0 border-neutral-200 hover:bg-gray-100"
-        @click="increaseQuantity(item)"
-      >
-        <IconAdd class="h-2 w-2" />
-      </button>
-    </div>
   </div>
 
   <div class="relative flex flex-1 flex-col justify-between">
     <div class="sm:col-gap-5 sm:flex">
-      <div class="pr-8 sm:pr-5">
+      <div class="pr-4 sm:pr-2">
         <p class="text-base font-semibold text-gray-900" v-text="item.product.name" />
         <div>
           <!-- variant options -->
@@ -96,10 +68,38 @@ async function decreaseQuantity(item: any) {
         </div>
       </div>
 
-      <div class="mt-4 flex items-end justify-between pt-10 sm:mt-0 sm:items-start sm:justify-end">
-        <p class="w-20 shrink-0 text-base font-semibold text-gray-900 sm:order-2 sm:text-right">
+      <div class="mt-4 flex flex-col justify-between pt-10 sm:mt-0">
+        <p class="w-20 shrink-0 text-base font-semibold text-gray-900 sm:text-right">
           {{ getFormattedPrice(item.total) }}
         </p>
+        <div class="mt-2 flex items-center">
+          <button
+            type="button"
+            :disabled="item.quantity === 1"
+            class="z-20 flex h-6 w-6 items-center justify-center border border-r-0 border-neutral-200 bg-gray-50 hover:bg-gray-100"
+            :class="{ 'cursor-not-allowed hover:bg-gray-50': item.quantity === 1 }"
+            @click="decreaseQuantity(item)"
+          >
+            <IconMinus class="h-2 w-2" />
+          </button>
+          <input
+            v-model="item.quantity"
+            type="number"
+            name="quantity"
+            class="focus:border-brand-600 focus:ring-brand-600 z-10 inline-flex h-6 w-10 border border-neutral-200 px-2 py-2 text-center text-xs [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+            inputmode="numeric"
+            disabled
+          />
+          <button
+            type="button"
+            :disabled="item.quantity === item.product.stock"
+            class="z-20 flex h-6 w-6 items-center justify-center border border-l-0 border-neutral-200 bg-gray-50 hover:bg-gray-100"
+            :class="{ 'cursor-not-allowed hover:bg-gray-50': item.quantity === item.product.stock }"
+            @click="increaseQuantity(item)"
+          >
+            <IconAdd class="h-2 w-2" />
+          </button>
+        </div>
       </div>
     </div>
 
