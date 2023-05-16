12ecc29fa72eedc327ea2ad917755b8871093f30
Adam Lee <adam@codelabx.ltd>
2023-05-16 01:08:17 +0100

    Start cart + send to friend modals

---
+++ src/components/core/store/cart/Modal.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    infoText: string
+    modalCartVisible: boolean
+  }>(),
+  {
+    infoText: 'Product added to your shopping cart',
+  },
+)
+
+const emit = defineEmits<{
+  (e: 'closeModal'): void
+}>()
+
+const images = props.item.attributes.images || []
+const thumbnail = images?.thumbnail.replace('conversions/', '').replace('-medium', '') || ''
+
+function closeModal() {
+  emit('closeModal')
+}
+</script>
+
+<template>
+  {{ thumbnail }}
+  <div class="modal" :class="{ 'modal-open': modalCartVisible }">
+    <div class="modal-box relative max-w-5xl p-0">
+      <div class="bg-brand-400 w-full p-4 text-lg font-semibold text-white">
+        <span class="mr-2">&#10003;</span> {{ infoText }}
+      </div>
+      <label for="addToCartModal" class="absolute right-3 top-3 z-50" @click="closeModal">
+        <button class="btn btn-sm btn-circle btn-outline text-white">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </label>
+      <div class="card lg:card-side bg-base-100 shadow-xl">
+        <figure class="m-0 w-96">
+          <img :src="thumbnail" :alt="item.attributes.name" />
+        </figure>
+        <div class="card-body">
+          <div class="flex justify-between gap-6">
+            <div class="text-lg font-semibold">
+              {{ item.attributes.name }}
+              <!-- <p class="mt-2 font-normal"><span>Size</span> 40 | <span>Color</span> Red</p> -->
+            </div>
+            <span class="text-brand-500 text-lg font-semibold">&euro; 380.00</span>
+          </div>
+          <CoreStoreCartItemsSummary class="pb-0 pt-4" />
+          <div class="text-center">
+            <NuxtLink
+              to="/"
+              class="hover:shadow-5xl inline-flex w-full items-center justify-center rounded-md px-6 py-3 text-lg font-semibold uppercase text-black shadow-2xl transition-all duration-200 ease-in-out focus:text-black focus:text-white focus:shadow"
+            >
+              <span>Continue Shopping</span>
+            </NuxtLink>
+          </div>
+          <div class="mt-2 text-center">
+            <NuxtLink
+              to="/checkout"
+              class="hover:bg-brand-500 inline-flex w-full items-center justify-center rounded-md bg-black px-3 py-3 text-lg font-semibold uppercase text-white transition-all duration-200 ease-in-out hover:text-white focus:text-white focus:shadow"
+            >
+              <span><span class="mr-2">&#10003;</span>Continue to Checkout</span>
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
