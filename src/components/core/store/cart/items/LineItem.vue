2546f9eb3b27465b98dc4775f1d881819f5e1d85
Adam Lee <adam@codelabx.ltd>
2023-05-20 00:21:29 +0100

    Create line item slot and extract quantity component

--- src/components/core/store/cart/items/LineItem.vue
+++ src/components/core/store/cart/items/LineItem.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
 import { Popconfirm } from 'ant-design-vue'
-import IconMinus from '~icons/carbon/subtract'
-import IconAdd from '~icons/carbon/add'
 import { useCartStore } from '#nuxt-store-core/store/cart'
 import useProductRoute from '#nuxt-store-core/composables/useProductRoute'
 
 const props = withDefaults(
   defineProps<{
-    variantWrapClasses?: string
-    variantOptionsListClasses?: string
-    rightColumnWrapClasses?: string
-    priceClasses?: string
-    quantityWrapClasses?: string
+    variantWrapClasses: string
+    variantOptionsListClasses: string
+    rightColumnWrapClasses: string
+    priceClasses: string
+    quantityWrapClasses: string
     item: Record<string, any>
   }>(),
   {
@@ -38,20 +36,6 @@ function getFormattedPrice(price: Ref<number> | number): string {
 
 async function removeLine(item: any) {
   await cartStore.removeLine(item.id)
-}
-
-async function increaseQuantity(item: any) {
-  if (item.quantity < item.product.stock) {
-    item.quantity++
-    await cartStore.updateQuantity(item.id, item.quantity)
-  }
-}
-
-async function decreaseQuantity(item: any) {
-  if (item.quantity > 1) {
-    item.quantity--
-    await cartStore.updateQuantity(item.id, item.quantity)
-  }
 }
 </script>
 
@@ -88,34 +72,9 @@ async function decreaseQuantity(item: any) {
         <p :class="priceClasses">
           {{ getFormattedPrice(item.total) }}
         </p>
-        <div :class="quantityWrapClasses">
-          <button
-            type="button"
-            :disabled="item.quantity === 1"
-            class="z-20 flex h-6 w-6 items-center justify-center border border-r-0 border-neutral-200 bg-gray-50 hover:bg-gray-100"
-            :class="{ 'cursor-not-allowed hover:bg-gray-50': item.quantity === 1 }"
-            @click="decreaseQuantity(item)"
-          >
-            <IconMinus class="h-2 w-2" />
-          </button>
-          <input
-            v-model="item.quantity"
-            type="number"
-            name="quantity"
-            class="focus:border-brand-600 focus:ring-brand-600 z-10 inline-flex h-6 w-10 border border-neutral-200 px-2 py-2 text-center text-xs [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
-            inputmode="numeric"
-            disabled
-          />
-          <button
-            type="button"
-            :disabled="item.quantity === item.product.stock"
-            class="z-20 flex h-6 w-6 items-center justify-center border border-l-0 border-neutral-200 bg-gray-50 hover:bg-gray-100"
-            :class="{ 'cursor-not-allowed hover:bg-gray-50': item.quantity === item.product.stock }"
-            @click="increaseQuantity(item)"
-          >
-            <IconAdd class="h-2 w-2" />
-          </button>
-        </div>
+        <slot name="line-item-quantity" :item="item">
+          <CoreStoreCartItemsLineItemQuantity :item="item" :quantity-wrap-classes="quantityWrapClasses" />
+        </slot>
       </div>
     </div>
 
---
+++ src/components/core/store/cart/items/LineItem/Quantity.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { useCartStore } from '#nuxt-store-core/store/cart'
+import IconAdd from '~icons/carbon/add'
+import IconSubtract from '~icons/carbon/subtract'
+
+defineProps<{
+  item: any
+  quantityWrapClasses: string
+}>()
+
+const cartStore = useCartStore()
+
+async function increaseQuantity(item: any) {
+  if (item.quantity < item.product.stock) {
+    item.quantity++
+    await cartStore.updateQuantity(item.id, item.quantity)
+  }
+}
+
+async function decreaseQuantity(item: any) {
+  if (item.quantity > 1) {
+    item.quantity--
+    await cartStore.updateQuantity(item.id, item.quantity)
+  }
+}
+</script>
+
+<template>
+  <div :class="quantityWrapClasses">
+    <button
+      type="button"
+      :disabled="item.quantity === 1"
+      class="z-20 flex h-6 w-6 items-center justify-center border border-r-0 border-neutral-200 bg-gray-50 hover:bg-gray-100"
+      :class="{ 'cursor-not-allowed hover:bg-gray-50': item.quantity === 1 }"
+      @click="decreaseQuantity(item)"
+    >
+      <IconSubtract class="h-2 w-2" />
+    </button>
+    <input
+      v-model="item.quantity"
+      type="number"
+      name="quantity"
+      class="focus:border-brand-600 focus:ring-brand-600 z-10 inline-flex h-6 w-10 border border-neutral-200 px-2 py-2 text-center text-xs [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+      inputmode="numeric"
+      disabled
+    />
+    <button
+      type="button"
+      :disabled="item.quantity === item.product.stock"
+      class="z-20 flex h-6 w-6 items-center justify-center border border-l-0 border-neutral-200 bg-gray-50 hover:bg-gray-100"
+      :class="{ 'cursor-not-allowed hover:bg-gray-50': item.quantity === item.product.stock }"
+      @click="increaseQuantity(item)"
+    >
+      <IconAdd class="h-2 w-2" />
+    </button>
+  </div>
+</template>
