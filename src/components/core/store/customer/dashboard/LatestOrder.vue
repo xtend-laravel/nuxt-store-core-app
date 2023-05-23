51ddf0b72c3bea3bf1bdc549aa3d2b4457e3763d
Boris Lepikhin <boris@lepikhin.com>
2023-05-23 13:00:10 -0700

    Extracted latest order classes

--- src/components/core/store/customer/dashboard/LatestOrder.vue
+++ src/components/core/store/customer/dashboard/LatestOrder.vue
@@ -5,8 +5,26 @@ import type { LatestOrder } from '#nuxt-store-core/store/customer/dashboard'
 withDefaults(
   defineProps<{
     latestOrder: LatestOrder | null
+    containerClasses?: string
+    bodyClasses?: string
+    titleClasses?: string
+    actionsClasses?: string
+    descriptionClasses?: string
+    lineWrapperClasses?: string
+    lineVariantContentClasses?: string
+    lineVariantWrapClasses?: string
+    lineVariantOptionsListClasses?: string
+    linePriceClasses?: string
+    lineQuantityWrapClasses?: string
   }>(),
-  {},
+  {
+    containerClasses: 'card card-side bg-base-100 mt-10 shadow-xl',
+    bodyClasses: 'card-body',
+    titleClasses: 'card-title flex justify-between',
+    actionsClasses: 'card-actions justify-end',
+    descriptionClasses: '',
+    lineWrapperClasses: 'flex flex-col space-y-3 py-6 text-left sm:flex-row sm:space-x-5 sm:space-y-0',
+  },
 )
 
 const { formatPrice } = useFormattedPrice()
@@ -18,30 +36,42 @@ function getFormattedPrice(price: Ref<number> | number): string {
 </script>
 
 <template>
-  <div v-if="latestOrder" class="card card-side bg-base-100 mt-10 shadow-xl">
-    <div class="card-body">
-      <h2 class="card-title flex justify-between">
+  <div v-if="latestOrder" :class="containerClasses">
+    <div :class="bodyClasses">
+      <h2 :class="titleClasses">
         Latest order
         <span class="text-brand-600">{{ getFormattedPrice(latestOrder.total) }}</span>
       </h2>
 
       <ul>
-        <li
-          v-for="item in latestOrder.products"
-          :key="item.id"
-          class="flex flex-col space-y-3 py-6 text-left sm:flex-row sm:space-x-5 sm:space-y-0"
-        >
+        <li v-for="item in latestOrder.products" :key="item.id" :class="lineWrapperClasses">
           <slot :key="item.id" name="override-items-line" :item="item">
-            <CoreStoreCustomerDashboardLatestOrderLineItem :key="item.id" :item="item" />
+            <CoreStoreCustomerDashboardLatestOrderLineItem
+              :key="item.id"
+              :wrapper-classes="lineWrapperClasses"
+              :variant-content-classes="lineVariantContentClasses"
+              :variant-wrap-classes="lineVariantWrapClasses"
+              :variant-options-list-classes="lineVariantOptionsListClasses"
+              :price-classes="linePriceClasses"
+              :quantity-wrap-classes="lineQuantityWrapClasses"
+              :item="item"
+            />
           </slot>
         </li>
       </ul>
 
-      <p>
-        Ordered on <span class="text-brand-600">{{ fromNow(latestOrder.created_at) }}</span>
+      <p :class="descriptionClasses">
+        <slot name="description">
+          Ordered <span class="text-brand-600">{{ fromNow(latestOrder.created_at) }}</span>
+        </slot>
       </p>
-      <div class="card-actions justify-end">
-        <button class="btn border-brand-500 bg-brand-500 hover:border-brand-600 hover:bg-brand-600">Track order</button>
+
+      <div :class="actionsClasses">
+        <slot name="actions">
+          <button class="btn border-brand-500 bg-brand-500 hover:border-brand-600 hover:bg-brand-600">
+            Track order
+          </button>
+        </slot>
       </div>
     </div>
   </div>
--- src/components/core/store/customer/dashboard/LatestOrder/LineItem.vue
+++ src/components/core/store/customer/dashboard/LatestOrder/LineItem.vue
@@ -3,9 +3,9 @@ import useProductRoute from '#nuxt-store-core/composables/useProductRoute'
 
 const props = withDefaults(
   defineProps<{
-    variantWrapClasses: string
-    variantOptionsListClasses: string
-    rightColumnWrapClasses: string
+    variantContentClasses?: string
+    variantWrapClasses?: string
+    variantOptionsListClasses?: string
     priceClasses: string
     quantityWrapClasses: string
     item: Record<string, any>
@@ -14,7 +14,6 @@ const props = withDefaults(
     variantContentClasses: 'sm:col-gap-5 sm:flex',
     variantOptionsListClasses: 'variant-options-list flex gap-2 text-xs',
     variantWrapClasses: 'variant-wrap mt-1 text-xs text-gray-500',
-    rightColumnWrapClasses: 'right-column mt-4 flex flex-col justify-between pt-10 sm:mt-0',
     priceClasses: 'price-style w-20 shrink-0 text-base font-semibold text-gray-900 sm:text-right',
     quantityWrapClasses: 'quantity-wrap mt-2 flex items-center',
   },
@@ -48,11 +47,13 @@ function getFormattedPrice(price: Ref<number> | number): string {
     <div :class="variantContentClasses">
       <div class="pr-4 sm:pr-2">
         <NuxtLink :to="url" class="text-base font-semibold text-gray-900" v-text="item.product.name" />
+
         <div :class="variantWrapClasses">
           <!-- variant options -->
           <ul :class="variantOptionsListClasses">
             <li v-for="optionValue in item.purchasable.values" :key="optionValue.id">
               <span><CoreStoreMultilingual :value="optionValue.option.name" />: </span>
+
               <span><CoreStoreMultilingual :value="optionValue.name" /></span>
             </li>
           </ul>
@@ -60,7 +61,7 @@ function getFormattedPrice(price: Ref<number> | number): string {
       </div>
     </div>
 
-    <div class="price-style w-20 shrink-0 text-base font-semibold text-gray-900">
+    <div :class="priceClasses">
       {{ getFormattedPrice(item.total) }}
     </div>
   </div>
