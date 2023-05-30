078f9b84cc59afdf519d08487b0f4d3f49526b63
Adam Lee <adam@codelabx.ltd>
2023-05-30 16:08:49 +0100

    Fix language switch styles

--- src/components/core/store/language-switch/LanguageSwitch.vue
+++ src/components/core/store/language-switch/LanguageSwitch.vue
@@ -5,7 +5,7 @@ import IconCheck from '~icons/feather/check'
 const { locale, locales, t } = useI18n()
 const switchLocalePath = useSwitchLocalePath()
 const availableLocales = computed(() => {
-  return locales.value.filter((i) => i.code !== locale.value)
+  return locales.value
 })
 const open = ref(false)
 </script>
@@ -14,19 +14,23 @@ const open = ref(false)
   <div>
     <Menu as="div" class="relative" @mouseenter="open = true" @mouseleave="open = false">
       <MenuButton class="cursor-pointer text-sm uppercase hover:opacity-75" v-text="locale" />
-      <div v-show="open" class="absolute left-0 top-0 z-10 -ml-2 mt-4 w-48 py-3">
+      <div v-show="open" class="absolute left-0 top-0 z-10 -ml-4 mt-4 w-48 py-3">
         <MenuItems static class="w-full rounded-md border border-neutral-50 bg-white py-1.5 shadow-lg">
           <div v-for="item in availableLocales" :key="item.code" class="flex w-full flex-col">
-            <MenuItem class="group px-4 py-2 text-sm font-light text-gray-400 hover:text-black">
+            <MenuItem
+              class="group px-4 py-2 text-sm font-light text-gray-400 hover:text-black"
+              :class="{ '!text-black': item.code === locale }"
+            >
               <NuxtLink
                 :to="switchLocalePath(item.code)"
                 class="flex w-full items-center justify-between gap-3 transition"
               >
                 <div class="flex items-center gap-2">
                   <img :src="`/images/${item.code}.webp`" :alt="item.code" class="inline h-3" />
-                  <span v-text="item.code" />
+                  <span v-text="item.name" />
                 </div>
-                <IconCheck class="hidden h-3 w-3 group-hover:inline-flex" />
+                <IconCheck v-if="item.code === locale" class="text-brand-500 h-4 w-4" />
+                <IconCheck v-else class="hidden h-4 w-4 group-hover:inline-flex" />
               </NuxtLink>
             </MenuItem>
           </div>
