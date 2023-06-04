cc62e42729d244dc2e90dc251b9603dd77daa79c
Boris Lepikhin <boris@lepikhin.com>
2023-06-03 22:18:39 -0700

    Set order address

---
+++ src/server/api/carts/set-address.ts
@@ -0,0 +1,18 @@
+import type { H3Event } from 'h3'
+import { readBody } from 'h3'
+import useNitroApi from '../../../composables/useNitroApi'
+
+export default defineEventHandler(async (event: H3Event) => {
+  const body = await readBody(event)
+
+  return await useNitroApi(
+    {
+      event,
+      action: '',
+      endpoint: `/api/restify/carts/${body.cartId}/public-actions?action=set-address-action`,
+      requiresAuth: false,
+      method: 'POST',
+    },
+    body,
+  )
+})
