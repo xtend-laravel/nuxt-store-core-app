c3654b13ab35ebad7b5d35ad6f2afa1111ed7441
Adam Lee <adam@codelabx.ltd>
2023-06-05 19:23:34 +0100

    Remove cart actions works direct with catch all server route

--- src/server/api/cart.ts
+++ src/server/api/cart.ts
@@ -2,8 +2,17 @@ import type { H3Event } from 'h3'
 import useNitroApi from '../../composables/useNitroApi'
 
 export default defineEventHandler(async (event: H3Event) => {
+  // @todo Refactor later
+  // return await useApi({
+  //   endpoint: `carts/getters/current-cart?sessionId=${event.context.session.id}`,
+  //   requiresAuth: false,
+  //   action: 'custom',
+  //   method: 'GET',
+  // })
+
   return await useNitroApi({
     event,
+    action: 'custom',
     endpoint: `/api/restify/carts/getters/current-cart?sessionId=${event.context.session.id}`,
   })
 })
