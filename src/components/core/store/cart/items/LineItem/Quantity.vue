d9f435188e82b459a9062d84cca4a3360fb21013
Adam Lee <adam@codelabx.ltd>
2023-06-05 18:53:49 +0100

    Refactor line item qty props + actions

--- src/components/core/store/cart/items/LineItem/Quantity.vue
+++ src/components/core/store/cart/items/LineItem/Quantity.vue
@@ -3,37 +3,33 @@ import { useCartStore } from '#nuxt-store-core/store/cart'
 import IconAdd from '~icons/carbon/add'
 import IconSubtract from '~icons/carbon/subtract'
 
-withDefaults(
-  defineProps<{
-    item: any
-    quantityWrapClasses: string
-    decreaseQuantityClasses: string
-    increaseQuantityClasses: string
-    quantityInputClasses: string
-  }>(),
-  {
-    decreaseQuantityClasses:
-      'z-20 flex h-6 w-6 items-center justify-center border border-r-0 border-neutral-200 bg-gray-50 hover:bg-gray-100',
-    increaseQuantityClasses:
-      'z-20 flex h-6 w-6 items-center justify-center border border-r-0 border-neutral-200 bg-gray-50 hover:bg-gray-100',
-    quantityInputClasses:
-      'focus:border-brand-600 focus:ring-brand-600 z-10 inline-flex h-6 w-10 border border-neutral-200 px-2 py-2 text-center text-xs [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none',
-  },
-)
+const props = defineProps<{
+  item: any
+  quantityWrapClasses: string
+  decreaseQuantityClasses: string
+  increaseQuantityClasses: string
+  quantityInputClasses: string
+}>()
+
+const {
+  decreaseQuantityClasses = 'z-20 flex h-6 w-6 items-center justify-center border border-r-0 border-neutral-200 bg-gray-50 hover:bg-gray-100',
+  increaseQuantityClasses = 'z-20 flex h-6 w-6 items-center justify-center border border-r-0 border-neutral-200 bg-gray-50 hover:bg-gray-100',
+  quantityInputClasses = 'focus:border-brand-600 focus:ring-brand-600 z-10 inline-flex h-6 w-10 border border-neutral-200 px-2 py-2 text-center text-xs [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none',
+} = props
 
 const cartStore = useCartStore()
 
 async function increaseQuantity(item: any) {
   if (item.quantity < item.product.stock) {
     item.quantity++
-    await cartStore.updateQuantity(item.id, item.quantity)
+    await cartStore.increaseQty(item.id, item.quantity)
   }
 }
 
 async function decreaseQuantity(item: any) {
   if (item.quantity > 1) {
     item.quantity--
-    await cartStore.updateQuantity(item.id, item.quantity)
+    await cartStore.decreaseQty(item.id, item.quantity)
   }
 }
 </script>
@@ -62,7 +58,7 @@ async function decreaseQuantity(item: any) {
       type="button"
       :disabled="item.quantity === item.product.stock"
       :class="[
-        decreaseQuantityClasses,
+        increaseQuantityClasses,
         item.quantity === item.product.stock ? 'cursor-not-allowed hover:bg-gray-50' : '',
       ]"
       @click="increaseQuantity(item)"
